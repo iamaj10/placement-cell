85526cf47a94f15b37a8aca4fd0929e92dfd0bb6
iamaj10 <dheerajangaj77@gmail.com>
2024-02-09 11:30:14 +0530

    added comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Import required modules and configurations
 const dbConfig = require("./config/database");
 const express = require("express");
 const cookieParser = require("cookie-parser");
@@ -6,28 +7,29 @@ const app = express();
 const port = 8001;
 const expressLayouts = require("express-ejs-layouts");
 
-// used for session cookie
+// Used for session cookie
 const session = require("express-session");
 const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategy");
 
+// MongoDB store for session
 const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const customMware = require("./config/middleware");
 
+// Connect to the MongoDB database
 dbConfig.connect();
 
+// Configure middleware
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(cookieParser());
-
 app.use(expressLayouts);
 
-// set up view engine
+// Set up view engine
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-// mongo store is used to store the session cookie in the db
+// Configure session
 app.use(
   session({
     name: "placement-cell",
@@ -38,33 +40,38 @@ app.use(
       maxAge: 1000 * 60 * 100,
     },
     store: MongoStore.create({
+      // Replace this URL with your MongoDB Atlas connection string
       mongoUrl:
         "mongodb+srv://dbUser:dbUserPassword@atlascluster.nesbr6t.mongodb.net/?retryWrites=true&w=majority",
       //in memory db
       // "mongodb://localhost:27017/placement_cell",
       autoRemove: "disabled",
     }),
+    // Log any errors during setup
     function(err) {
       console.log(err || "connect-mongodb setup ok");
     },
   })
 );
 
+// Initialize passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-// sets the authenticated user in the response
+// Sets the authenticated user in the response
 app.use(passport.setAuthenticatedUser);
 
+// Configure flash messages
 app.use(flash());
 app.use(customMware.setFlash);
 
-// use express router
+// Use express router
 app.use("/", require("./routes"));
 
+// Start the server
 app.listen(port, (err) => {
   if (err) {
     console.log(`Error in running the server: ${err}`);
   }
-  console.log(`server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
